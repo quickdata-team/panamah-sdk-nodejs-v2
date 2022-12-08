ba5a49f6a15e3064853b72f86c14bafbf0b87d1f
pdavinunes <p.davi.nunes@gmail.com>
2022-12-08 18:15:00 -0300

    feat(pan-688): add novos metodos na FsLib

--- src/infra/externalInterfaces/storageLib/fsLib.ts
+++ src/infra/externalInterfaces/storageLib/fsLib.ts
@@ -1,4 +1,11 @@
-import { existsSync, mkdirSync, writeFileSync, readdirSync } from 'fs';
+import {
+  existsSync,
+  mkdirSync,
+  writeFileSync,
+  readdirSync,
+  createWriteStream,
+  readFileSync,
+} from 'fs';
 
 export class FsLib {
   static isFileExists(filePath: string): boolean {
@@ -13,11 +20,19 @@ export class FsLib {
     mkdirSync(dirPath);
   }
 
-  static saveFile(filePath: string, fileContent: string): void {
+  static saveFile(filePath: string, fileContent: string | Buffer): void {
     writeFileSync(filePath, fileContent);
   }
 
   static readDirFiles(dirPath: string): string[] {
     return readdirSync(dirPath);
+  }
+
+  static writeStream(filePath: string) {
+    return createWriteStream(filePath);
+  }
+
+  static readFile(filePath: string) {
+    return readFileSync(filePath);
   }
 }
