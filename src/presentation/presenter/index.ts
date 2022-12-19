071ac823e84b5fd7c62475b1fac3959676520c04
Joao Bitelo <joao.bitelo@quickdata.com.br>
2022-12-19 14:00:12 -0300

    refactor(PAN-705): corrige apresentação e adapter dos casos de uso

--- src/presentation/presenter/index.ts
+++ src/presentation/presenter/index.ts
@@ -1,69 +1,13 @@
-import { Compressor, StreamingFlow, AdministrativeUseCase } from '@useCases';
-import {
-  BaseError,
-  XMLBadRequestError,
-  NFEBadRequestError,
-  NFEschemasBadRequestError,
-} from '@errors';
-
-export type IinitParameters = {
-  username: string;
-  password: string;
-};
-
-const streamingFlow = new StreamingFlow();
-/**
- * Inicia o fluxo de envio
- * @export
- * @param {IinitParameters} {
- *   username,
- *   password,
- * }
- * @return {*}  {Promise<void>}
- */
-export async function init({
-  username,
-  password,
-}: IinitParameters): Promise<void> {
-  return streamingFlow.init({ username, password });
-}
-
-/**
- * Para o fluxo de envio e envia o que estiver pendente
- * @export
- * @return {*}  {void}
- */
-export async function stop(): Promise<void> {
-  return streamingFlow.terminate();
-}
-
-const compressor = new Compressor(streamingFlow);
-
-/**
- * Método para salvar e enviar um XML de NFE
- * @export
- * @param {string} nfeContent
- * @param {boolean} [fromPath=true]
- * @return {*}  {(void
- *   | XMLBadRequestError
- *   | NFEBadRequestError
- *   | NFEschemasBadRequestError
- *   | BaseError)}
- */
-export async function send(
-  nfeContent: string,
-  fromPath: boolean = true
-): Promise<
-  | void
-  | XMLBadRequestError
-  | NFEBadRequestError
-  | NFEschemasBadRequestError
-  | BaseError
-> {
-  return compressor.send(nfeContent, fromPath);
-}
-
-export const administrative = new AdministrativeUseCase();
+export {
+  init as PanamahInit,
+  send as PanamahSend,
+  stop as PanamahTerminate,
+  validateXml as PanamahValidateXML,
+  createSubscriber as PanamahCreateSubscriber,
+  getSubscriber as PanamahGetSubscriber,
+  updateSubscriber as PanamahUpdateSubscriber,
+  deleteSubscriber as PanamahDeleteSubscriber,
+} from '../adapters';
 
 export {
   UnprocessableEntityError,
@@ -84,4 +28,4 @@ export {
   NFEschemasBadRequestError,
   SubscriberConflictError,
   BaseError,
-} from '../../core/entities/erro';
+} from '@errors';
