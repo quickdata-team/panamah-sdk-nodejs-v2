3698150fcc268d6f05b6618e9c5c45fdf96c10b5
Joao Bitelo <joao.bitelo@quickdata.com.br>
2022-12-29 16:48:02 -0300

    refactor(PAN-709): simplifica logica de mutex, agora usa setTimeoutId e clearTimeout

--- src/core/entities/mutex.ts
+++ src/core/entities/mutex.ts
@@ -1,29 +1,24 @@
 /* eslint-disable no-await-in-loop */
 
 type IRunningStatus = {
-  terminate?: boolean; // Force stop flag
-  mutex?: boolean; // Se esta sendo executado uma operação bloqueante
-  mutexWaitMs?: number; // Intervalo de espera em caso de operações bloqueantes
-  streamingLoop?: boolean; // Flag de running do streamingloop
-  streamingLoopIntervalMs?: number; // Intervalo de envio do SDK para a API
-  refreshTokenLoop?: boolean; // Flag de running do refreshToken
-  refreshTokenLoopIntervalMs?: number;
-  compress?: boolean;
+  mutexWaitMs: number; // Intervalo de espera em caso de operações bloqueantes
+  streamingLoop: boolean; // Flag de running do streamingloop
+  refreshTokenLoop: boolean; // Flag de running do refreshToken
+  compress: boolean;
 };
 export class Mutex {
   private runningStatus: IRunningStatus = {
-    terminate: true, // Force stop flag
-    mutex: false, // Se esta sendo executado uma operação bloqueante
-    mutexWaitMs: 1000, // Intervalo de espera em caso de operações bloqueantes
-    streamingLoop: false, // Flag de running do streamingloop
-    refreshTokenLoop: false, // Flag de running do refreshToken
+    mutexWaitMs: 1000,
+    streamingLoop: false,
+    refreshTokenLoop: false,
+    compress: false,
   };
 
   public getStatus() {
     return this.runningStatus;
   }
 
-  public setStatus(newRunningStatus: IRunningStatus) {
+  public setStatus(newRunningStatus: any) {
     this.runningStatus = {
       ...this.runningStatus,
       ...newRunningStatus,
@@ -37,64 +32,50 @@ export class Mutex {
     );
   }
 
+  public stillRunning(): boolean {
+    const status = this.getStatus();
+    return status.streamingLoop || status.refreshTokenLoop || status.compress;
+  }
+
   public async checkForBlocking(): Promise<void> {
-    while (this.runningStatus.mutex) {
+    while (this.stillRunning()) {
       await this.delay();
     }
   }
 
   public blockStream() {
     this.setStatus({
-      mutex: true,
       streamingLoop: true,
     });
   }
 
   public unblockStream() {
     this.setStatus({
-      mutex: false,
       streamingLoop: false,
     });
   }
 
   public blockRefreshToken() {
     this.setStatus({
-      mutex: true,
       refreshTokenLoop: true,
     });
   }
 
   public unblockRefreshToken() {
     this.setStatus({
-      mutex: false,
       refreshTokenLoop: false,
     });
   }
 
   public blockCompress() {
     this.setStatus({
-      mutex: true,
       compress: true,
     });
   }
 
   public unblockCompress() {
     this.setStatus({
-      mutex: false,
       compress: false,
     });
-  }
-
-  public terminate() {
-    this.setStatus({ terminate: true });
-  }
-
-  public stillRunning() {
-    const status = this.getStatus();
-    return status.streamingLoop || status.refreshTokenLoop || status.compress;
-  }
-
-  public isNotTerminated() {
-    return !this.runningStatus.terminate;
   }
 }
